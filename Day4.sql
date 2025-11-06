46d758fc59e733079a728d84b8d751bda3d32fda
SANGEETHA_Dhanasekar <72749005+sangeethadhanasekar@users.noreply.github.com>
2025-11-06 22:41:49 +0530

    Day4

---
+++ Day4.sql
@@ -0,0 +1,12 @@
+-- 1. Display the first 5 patients from the patients table.
+select * from patients LIMIT 5;
+-- 2. Show patients 11-20 using OFFSET.
+select * from patients LIMIT 10 OFFSET 10
+-- 3. Get the 10 most recent patient admissions based on arrival_date.
+select * from patients  ORDER BY arrival_date DESC LIMIT 10;
+
+-- ### Daily Challenge:
+-- **Question:** Find the 3rd to 7th highest patient satisfaction scores from the patients table, 
+-- showing patient_id, name, service, and satisfaction. Display only these 5 records.
+select patient_id,name,service,satisfaction from patients LIMIT 5 OFFSET 3;
+
