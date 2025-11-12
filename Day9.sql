7dc1d35c5d0c831eae478cf30b3deab96af19353
SANGEETHA_Dhanasekar <72749005+sangeethadhanasekar@users.noreply.github.com>
2025-11-12 23:36:44 +0530

    day9

--- Day9.sql
+++ Day9.sql
@@ -13,5 +13,7 @@ select * , EXTRACT(MONTH from arrival_date) as month_of_arrival from patients WH
 -- Calculate the average length of stay (in days) for each service, showing only services
 -- where the average stay is more than 7 days. Also show the count of patients and order by average stay descending.
 select * from patients;
+-- select service, COUNT(patient_id) as count_of_patients, ROUND(AVG (departure_date - arrival_date),0)as AVG_length_of_stay  from patients 
+-- GROUP BY service HAVING ROUND(AVG (departure_date - arrival_date),0) >7;
 select service, COUNT(patient_id) as count_of_patients, ROUND(AVG (departure_date - arrival_date),0)as AVG_length_of_stay  from patients 
-GROUP BY service HAVING ROUND(AVG (departure_date - arrival_date),0) >7;
+GROUP BY service HAVING ROUND(AVG (departure_date - arrival_date),0) >7 ORDER BY AVG_length_of_stay DESC ;
